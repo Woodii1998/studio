948ece697fb730447592cc95cb8adc0f6bfbe9b7
Woodii <junhui.li@coscene.io>
2023-01-07 18:08:22 +0800

    fix: Fix the Chinese encoding error caused by style

--- packages/studio-base/src/util/sharedStyleConstants.ts
+++ packages/studio-base/src/util/sharedStyleConstants.ts
@@ -1,6 +1,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
+import i18next from "i18next";
 
 export const fonts = {
   // We explicitly avoid fallback fonts (such as 'monospace') here to work around a bug in
@@ -13,5 +14,6 @@ export const fonts = {
   SANS_SERIF: "'Inter'",
   SANS_SERIF_FEATURE_SETTINGS:
     // enable font features https://rsms.me/inter/lab
-    "'cv08', 'cv10', 'tnum'",
+    // This style affects Chinese encoding and should be disabled when the current language is Chinese.
+    i18next.language === "zh" ? "" : "'cv08', 'cv10', 'tnum'",
 };
