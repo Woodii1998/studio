fffc48638e4484e3b0dacd1bf6b67f10e6a9a59e
Woodii <33593572+Woodii1998@users.noreply.github.com>
2023-09-07 13:38:59 +0800

    fix: lint

--- packages/studio-base/src/components/TopicList/TopicList.tsx
+++ packages/studio-base/src/components/TopicList/TopicList.tsx
@@ -156,7 +156,7 @@ export function TopicList(): JSX.Element {
             endAdornment: undebouncedFilterText && (
               <IconButton
                 size="small"
-title={t("topics:clearFilter")}
+                title={t("topics:clearFilter")}
                 onClick={() => {
                   setFilterText("");
                 }}
